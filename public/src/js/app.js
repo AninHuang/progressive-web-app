5a13fc4a9edf57dd4d799e40a5f31a7867088b07
AninHuang <lifeanne19904@gmail.com>
2019-10-19 23:57:56 +0800

    Add comments

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -2,8 +2,10 @@
 var deferredPrompt;
 
 if ('serviceWorker' in navigator) {
+  // Register a SW (only work on pages served via https)
+  // (localhost is an exception to make dev easier)
   navigator.serviceWorker
-    .register('/sw.js', {scope: '/help/'})
+    .register('/sw.js', {scope: '/help/'}) // SW only controls the /help/* pages
     .then(function() {
       console.log('service worker registered!');
     });
