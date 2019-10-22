a9edce3261f6d020c6b9e1ce2ce3d88e9f23679d
AninHuang <lifeanne19904@gmail.com>
2019-10-22 23:13:19 +0800

    Add comments

--- public/sw.js
+++ public/sw.js
@@ -7,7 +7,10 @@ self.addEventListener('activate', function(event) {
   return self.clients.claim();
 });
 
+// fetch event uses the browser default for non-GET requests.
+// For GET requests it tries to return a match in the cache, and falls back to the network.
 self.addEventListener('fetch', function(event) {
   console.log('Fetching sth...', event);
+  // Prevent the default, and handle the request ourselves.
   event.respondWith(fetch(event.request));
 });
